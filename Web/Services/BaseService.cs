ae0eb48a00c8b7a6251cb4425e1af5d4a58f6d50
pradeep23.akg <pradeep23.akg@gmail.com>
2021-09-27 00:55:06 +0530

    ..

--- Web/Services/BaseService.cs
+++ Web/Services/BaseService.cs
@@ -1,8 +1,11 @@
-﻿using OnlineApp.Web.UI.IServices;
+﻿using Newtonsoft.Json;
+using OnlineApp.Web.UI.IServices;
 using OnlineApp.Web.UI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineApp.Web.UI.Services
@@ -11,14 +14,70 @@ namespace OnlineApp.Web.UI.Services
     {
         public ResponseDto responseModel { get; set; }
 
+        public IHttpClientFactory httpClient { get; set; }
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            httpClient = httpClientFactory;
+            this.responseModel = new ResponseDto();
+        }
+
         public void Dispose()
         {
             GC.SuppressFinalize(true);
         }
 
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var client = httpClient.CreateClient("ProductAPI");
+                client.DefaultRequestHeaders.Clear();
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept","application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8,"application/json");
+                }
+                HttpResponseMessage apiResponse = null;
+                switch(apiRequest.ApiType)
+                {
+                    
+                    case StaticConfiguration.ApiType.POST:
+                                                        message.Method = HttpMethod.Post;
+                                                        break;
+                    case StaticConfiguration.ApiType.DELETE:
+                                                        message.Method = HttpMethod.Delete;
+                                                        break;
+                    case StaticConfiguration.ApiType.PUT:
+                                                        message.Method = HttpMethod.Put;
+                                                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+
+
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessage = new List<string>
+                    {
+                        Convert.ToString(ex.Message)
+                    }
+                    , IsSuccess = false
+                };
+                 var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDto;
+
+            }
         }
     }
 }
---
+++ Web/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using OnlineApp.Web.UI.IServices;
+using OnlineApp.Web.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnlineApp.Web.UI.Services
+{
+    public class ProductService : BaseService, IProductService
+    {
+        IHttpClientFactory httpClientFactory;
+        public ProductService(IHttpClientFactory _httpClientFactory):base(_httpClientFactory)
+        {
+            httpClientFactory = _httpClientFactory;
+        }
+        public Task<T> CreateProductAsync<T>(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteProductAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllProductsAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllProductsByIdAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateProductAsync<T>(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
