5fca3570addd9b6bfbea7d6b65f985ca27455a77
pradeep23.akg <pradeep23.akg@gmail.com>
2021-09-26 23:24:19 +0530

    ....

---
+++ Web/StaticConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineApp.Web.UI
+{
+    public static class StaticConfiguration
+    {
+        public static string ProductAPIBase { get; set; }
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
